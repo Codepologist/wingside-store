3e5138aebab68cb9491d48314f4aeeae83339a2e
ONeal Ombu <ombu.oneal@yahoo.com>
2023-09-21 20:51:29 +0100

    Modified export Register and Login page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,5 +4,7 @@ import OrderNow from "./orderNow/OrderNow";
 import Cart from "./cart/Cart";
 import OrderHistory from "./orderHistory/OrderHistory";
 import Contact from "./contact/Contact";
+import Register from "./register/Register";
+import Login from "./login/Login";
 
-export { Home, Admin, OrderNow, Cart, OrderHistory, Contact };
+export { Home, Admin, OrderNow, Cart, OrderHistory, Contact, Register, Login };
