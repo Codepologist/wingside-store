dce7dde0e4a73876733bab18a7e22eeb14189f33
ONeal Ombu <ombu.oneal@yahoo.com>
2023-10-05 12:10:19 +0100

    modified

--- backend/firebase/config.js
+++ backend/firebase/config.js
@@ -15,7 +15,6 @@ export const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
